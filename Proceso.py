efc7733d43fa924b4e1131a375eecf499ac33e1c
piere0405 <jampierlml@gmail.com>
2026-03-23 10:28:09 -0500

    numeros
    numeros

--- Proceso.py
+++ Proceso.py
@@ -8,7 +8,9 @@ def buscar(x):
 
         if len(x) == 9:
             r = df.loc[
-                df["NUMERO 1"].astype(str) == x,
+                (df["NUMERO 1"].astype(str) == x)|  
+                (df["NUMERO 2"].astype(str) == x)| 
+                (df["NUMERO 3"].astype(str) == x) ,
                 ["DNI", "NOMBRE", "RAZON_SOCIAL", "NUMERO 1"]
             ]
             return r
