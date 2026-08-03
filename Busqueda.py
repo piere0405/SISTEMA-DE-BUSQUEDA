8a017bac7cd51dd09592cce3b069753b65b3381c
piere0405 <jampierlml@gmail.com>
2026-08-03 11:29:20 -0500

    AGOSTO

--- Busqueda.py
+++ Busqueda.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import Proceso as pc
 
-st.title("🔍 HERRAMIENTA INFORMATIVA ")
+st.title("🔍 HERRAMIENTA INFORMATIVA - AGOSTO ")
 
 tab1, tab2 = st.tabs(["👤 Datos Cliente", "🎯 Verificacion Campaña"])
 
